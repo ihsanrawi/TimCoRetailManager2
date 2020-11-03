149f833849e013191823268c0ed11696dfe19f65
Ihsan <ihsanrawi@gmail.com>
2020-11-03 16:27:35 +0000

    WPF Login Form Creation
    Created LoginView
    Fixed PasswordBox not connect properly by adding PasswordBoxHelper class
    
    # Conflicts:
    #	README.md
    
    # Conflicts:
    #	README.md

--- TRMDesktopUI/ViewModels/ShellViewModel.cs
+++ TRMDesktopUI/ViewModels/ShellViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Caliburn.Micro;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,13 @@ using System.Threading.Tasks;
 
 namespace TRMDesktopUI.ViewModels
 {
-    public class ShellViewModel
+    public class ShellViewModel : Conductor<object>
     {
+        private LoginViewModel _loginVM;
+        public ShellViewModel(LoginViewModel loginVM)
+        {
+            _loginVM = loginVM;
+            ActivateItem(_loginVM);
+        }
     }
 }
