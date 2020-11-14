667b644e663051b5a1748b73f457629ccb5dc349
ihsan rawi <ihsanrawi@gmail.com>
2020-11-14 20:15:33 +0000

    Remove old API as all controllers had been moved to the new TRMAPI

--- TRMDataManager/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace TRMDataManager.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
