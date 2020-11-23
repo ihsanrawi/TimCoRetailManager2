952c0567da01fb31f8fec2fba82c69e88da624f1
Ihsan <ihsanrawi@gmail.com>
2020-11-23 16:27:47 +0000

    Fix table name showing warning

--- TRMData/dbo/Stored Procedures/spSale_SaleReport.sql
+++ TRMData/dbo/Stored Procedures/spSale_SaleReport.sql
@@ -6,5 +6,5 @@ BEGIN
 	SELECT [S].[SaleDate], [S].[SubTotal], [S].[Tax], [S].[Total]
 		, U.FirstName, U.LastName, U.EmailAddress
 	FROM dbo.Sale S
-	INNER JOIN dbo.[USER] U ON U.Id = S.CashierId
+	INNER JOIN dbo.[User] U ON U.Id = S.CashierId
 END
